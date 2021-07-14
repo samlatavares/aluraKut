eeac5bc29d9583f327adf227095c0f45846a18f1
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2021-07-13 22:12:23 -0300

    Implementing communities box and form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSideBar(propriedades) {
   return (
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+      <p>
+        <a className="boxLink" href={`https://github.com/${propriedades.githubUser}/`}>
+          @{propriedades.githubUser}
+        </a>
+      </p>
+
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
@@ -14,10 +24,15 @@ function ProfileSideBar(propriedades) {
 export default function Home() {
   const githubUser = 'samlatavares';
   const pessoasFavoritas = ['yanpitangui', 'danyAmaral', 'jorgesoprani', 'juunegreiros', 'peas', 'omariosouto'];
+  const [comunidades, setComunidades] = React.useState([{
+    id: '01011111',
+    title: 'Eu odeio acordar cedo',
+    image: 'https://live.staticflickr.com/7226/6857485718_3a41e5dc0b_n.jpg'
+  }]);
 
   return (
     <>
-      <AlurakutMenu />
+      <AlurakutMenu githubUser={githubUser} />
       <MainGrid>
         <div className="profileArea" style={{ gridArea: 'profileArea' }}>
           <ProfileSideBar githubUser={githubUser} />
@@ -26,6 +41,41 @@ export default function Home() {
           <Box>
             <h1 className="title">Bem vindo(a)</h1>
             <OrkutNostalgicIconSet />
+          </Box>
+          <Box>
+            <h2 className="subtitle">O que você deseja fazer?</h2>
+            <form onSubmit={function handleCreateCommunity(e) {
+              e.preventDefault();
+
+              const dadosInformados = new FormData(e.target);
+              const comunidadesAtualizadas = [...comunidades, 'teste2'];
+              const comunidade = {
+                id: new Date().toISOString,
+                titulo: dadosInformados.get('title'),
+                image: dadosInformados.get('image')
+              }
+              setComunidades(comunidadesAtualizadas, comunidade);
+            }}>
+              <div>
+                <input
+                  placeHolder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeHolder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                  type="text"
+                />
+              </div>
+              <button>
+                Criar comunidade
+            </button>
+            </form>
           </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
@@ -46,7 +96,23 @@ export default function Home() {
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
-          <Box>Comunidades</Box>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Comunidades
+            </h2>
+            <ul>
+              {comunidades.map((comunidade) => {
+                return (
+                  <li>
+                    <a href={`/users/${comunidade.title}`} key={comunidade.id}>
+                      <img src={comunidade.image} />
+                      <span>{comunidade.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
